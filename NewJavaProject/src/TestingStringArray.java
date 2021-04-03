c1f030a5759a62244d5cfe5712b8a5f08fe27b93
yinyinminnaing <yinminfeb@gmail.com>
2021-04-03 23:21:10 +0630

    testing create new branch

--- NewJavaProject/src/TestingStringArray.java
+++ NewJavaProject/src/TestingStringArray.java
@@ -2,6 +2,6 @@
 public class TestingStringArray {
 
 	public static void main(String[] args) {
-		System.out.println("Testing.......");
+		System.out.println("Testing.......and Testing Branches");
 	}
 }
