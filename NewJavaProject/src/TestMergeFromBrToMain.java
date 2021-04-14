2d01ba15cd5bf9c118fbc2fa7bedcb8c27ad0623
yinyinminnaing <yinminfeb@gmail.com>
2021-04-14 20:11:41 +0630

    test dev

--- NewJavaProject/src/TestMergeFromBrToMain.java
+++ NewJavaProject/src/TestMergeFromBrToMain.java
@@ -3,5 +3,6 @@ public class TestMergeFromBrToMain {
 
 	public static void main(String[] args) {
 		System.out.println("Testing merge to main");
+		System.out.println("Testing development branch different with master");
 	}
 }
