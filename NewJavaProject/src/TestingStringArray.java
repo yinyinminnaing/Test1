6505a3ea95be138a944a9276672a683478d34c51
yinyinminnaing <yinminfeb@gmail.com>
2021-06-14 08:50:00 +0630

    commit to master

--- NewJavaProject/src/TestingStringArray.java
+++ NewJavaProject/src/TestingStringArray.java
@@ -3,6 +3,8 @@ public class TestingStringArray {
 
 	public static void main(String[] args) {
 		System.out.println("Testing.......and Testing Branches");
-		System.out.println("Testing.......and Create New Branches");
+		// testing create branches
+		System.out.println("Testing.......and Creating Branches");
+
 	}
 }
