de086805fc1ad43dfc4f5e728343ec8d024d2aeb
yinyinminnaing <yinminfeb@gmail.com>
2021-06-22 15:46:37 +0630

    test after create tag

--- NewJavaProject/src/TestingFile1.java
+++ NewJavaProject/src/TestingFile1.java
@@ -4,6 +4,8 @@ public class TestingFile1 {
 	public static void main(String[] args) {
 		System.out.println("Testing New Class in Master Branch");
 
+		System.out.println("Testing after create Tag PROD.1.2.1");
+
 	}
 
 }
